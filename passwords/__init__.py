9edeb4cd756dbac39a2c66441a3c46bb03e82237
Dharwin Perez <dhararon@gmail.com>
2024-02-08 16:55:09 -0600

    build: Change version file

--- passwords/__init__.py
+++ passwords/__init__.py
@@ -1,18 +1,8 @@
-VERSION = (0, 1, 0, "final", 0)
+'''
+X.YaN   # Alpha release
+X.YbN   # Beta release
+X.YrcN  # Release Candidate
+X.Y     # Final release
+'''
 
-
-def get_version():
-    """Get package version."""
-    string_version: str = ""
-    if VERSION[3] == "final":
-        string_version = f"{VERSION[0]}.{VERSION[1]}.{VERSION[2]}"
-    elif VERSION[3] == "dev":
-        if VERSION[2] == 0:
-            string_version = f"{VERSION[0]}.{VERSION[1]}.{VERSION[3]}{VERSION[4]}"
-        string_version = f"{VERSION[0]}.{VERSION[1]}.{VERSION[2]}.{VERSION[3]}{VERSION[4]}"
-    else:
-        string_version = f"{VERSION[0]}.{VERSION[1]}.{VERSION[2]}{VERSION[3]}"
-    return string_version
-
-
-__version__ = get_version()
+__version__ = "0.1.0"
