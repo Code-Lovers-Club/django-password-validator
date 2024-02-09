26a5cc2f9acc85ba642a3264acd42af4a350815e
Dharwin Perez <dhararon@gmail.com>
2024-02-08 22:47:14 -0600

    feat: update version and move code to src path

--- src/passwords/__init__.py
+++ src/passwords/__init__.py
@@ -1,8 +1,9 @@
+from  importlib.metadata import version
+
+__version__ = version("django-password-validator")
 """
 X.YaN   # Alpha release
 X.YbN   # Beta release
 X.YrcN  # Release Candidate
 X.Y     # Final release
 """
-
-__version__ = "0.1.2"
