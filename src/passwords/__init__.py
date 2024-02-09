ee79ca1096801e98dde970eebcd4b69437289a7c
Dharwin Perez <dhararon@gmail.com>
2024-02-08 22:56:34 -0600

    build: Fix get version from pyproject

--- src/passwords/__init__.py
+++ src/passwords/__init__.py
@@ -2,7 +2,6 @@ from  importlib.metadata import version
 
 __version__ = version("django-password-validator")
 """
-X.YaN   # Alpha release
 X.YbN   # Beta release
 X.YrcN  # Release Candidate
 X.Y     # Final release
